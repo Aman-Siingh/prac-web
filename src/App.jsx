a58d44e9f3af9056dc3d665ce751e6878c155434
Aman-Siingh <amansiingh15@gmail.com>
2024-01-01 17:09:02 +0530

    Added Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Homepage';
+import AboutPage from './pages/Aboutpage'; // import your about page component
+import Services from './pages/Servicespage';
+import Gallery from './pages/Gallery';
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <div>
+        
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/services" component={Services} />
+        <Route path="/gallery" component={Gallery} />
+        {/* Add more Route components for additional pages */}
+      </div>
+    </Router>
+  );
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+
+        <div>
+            <h1>AMAN </h1>
+        </div>
+     );
+}
+ 
+export default Home;
